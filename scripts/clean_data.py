80d5cd83f38b3049a618ecea5df41ee65037d12e
Megan Rashid <megan.c.rashid@gmail.com>
2024-09-21 20:07:25 +0300

    tested and passed data cleanup

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -22,4 +22,30 @@ print(f"Database path: {db_path}")
 conn = db.connect_to_db(db_path)
 
 # check table structure and column names
+
+
+#rename columns
+columns_to_rename = {'Unnamed: 0':'Company_ID',
+                     'year founded':'Year_Founded',
+                     'industry':'Industry',
+                     'size range':'Size_Range',
+                     'locality':'Locality',
+                     'country':'Country',
+                     'linkedin url': 'Linkedin_URL',
+                     'current employee estimate':'Current_Employee_Estimate',
+                     'total employee estimate':'Total_Employee_Estimate'
+                     }
+
+db.rename_columns(conn, 'CompanyDataset', columns_to_rename)
+
+# add indexes to both tables
+indexes_to_create = {
+    'CompanyDataset': ['Company_ID','CompanyName', 'Website','Industry','Size_Range', 'Country','Current_Employee_Estimate','Total_Employee_Estimate']
+    ,'CompanyClassification': ['Category', 'CompanyName', 'Website']
+}
+
+db.create_indexes(conn, indexes_to_create)
+
+# check table structure and column names
+
 print(db.run_query(conn,"PRAGMA table_info('CompanyDataset');"))
