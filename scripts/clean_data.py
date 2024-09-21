ab6941655976ad7aae4524179e7e62e6bd5d4a25
Megan Rashid <megan.c.rashid@gmail.com>
2024-09-21 20:02:16 +0300

    init

---
+++ scripts/clean_data.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import pandas as pd
+
+# Construct the absolute path to the utils directory and append it to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
+
+# Now you can import the db_utils module
+import db_utils as db
+
+# set up params
+
+# get the base directory
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+print(f"base directory: {base_dir}")
+
+# get data path
+db_path = os.path.join(base_dir, 'data', 'combined_data.db')
+print(f"Database path: {db_path}")
+
+#connect to the database
+conn = db.connect_to_db(db_path)
+
+# check table structure and column names
+print(db.run_query(conn,"PRAGMA table_info('CompanyDataset');"))
